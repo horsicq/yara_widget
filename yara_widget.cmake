e4c70e7c5cec771c3cfefa23c480ec6419cb3ca5
horsicq <horsicq@gmail.com>
2024-02-28 19:09:08 +0100

    Update file(s): 2024-02-28

--- yara_widget.cmake
+++ yara_widget.cmake
@@ -6,10 +6,14 @@ set(YARA_WIDGET_SOURCES
     ${FORMRESULT_SOURCES}
     ${DIALOGTEXTINFO_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/yara_widget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/yara_widget.h
     ${CMAKE_CURRENT_LIST_DIR}/yara_widget.ui
     ${CMAKE_CURRENT_LIST_DIR}/yarawidgetadvanced.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/yarawidgetadvanced.h
     ${CMAKE_CURRENT_LIST_DIR}/yarawidgetadvanced.ui
     ${CMAKE_CURRENT_LIST_DIR}/xyaradialogprocess.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xyaradialogprocess.h
     ${CMAKE_CURRENT_LIST_DIR}/dialogyarawidgetadvanced.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialogyarawidgetadvanced.h
     ${CMAKE_CURRENT_LIST_DIR}/dialogyarawidgetadvanced.ui
 )
